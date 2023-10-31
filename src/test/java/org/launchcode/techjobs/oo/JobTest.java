96b8c0b1cc0dace44adc0f5cbccace198597cfbb
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2023-10-31 15:38:38 -0500

    Deleted extra oo folder from repo

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -15,23 +15,25 @@ public class JobTest {
 
 //    @Test
 //    public void testJobConstructionSetsAllFields() {
-//        Job test_job_fields = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+//        Job test_job_fields = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Perstistence"));
 //    }
 
 
-        @Test
-        public void testJobConstructionSetsAllFields() {
-            Job test_job_fields = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-            assertEquals("Product tester", test_job_fields.getName());
-            assertEquals("ACME", test_job_fields.getEmployer().getValue());
-            assertEquals("Desert", test_job_fields.getLocation().getValue());
-            assertEquals("Quality control", test_job_fields.getPositionType().getValue());
-            assertEquals("Persistence", test_job_fields.getCoreCompetency().getValue());
+    @Test
+    public void testJobConstructionSetsAllFields() {
+        Job test_job_fields = new Job("Product tester", new Employer("ACME").toString(), new Location("Desert").toString(), new PositionType("Quality control").toString(), new CoreCompetency("Persistence").toString());
+        assertEquals("Product tester", test_job_fields.getName());
+        assertEquals("ACME", test_job_fields.getEmployer().getValue());
+        assertEquals("Desert", test_job_fields.getLocation().getValue());
+        assertEquals("Quality control", test_job_fields.getPositionType().getValue());
+        assertEquals("Persistence", test_job_fields.getCoreCompetency().getValue());
 
-            assertTrue("Product tester", true);
-            assertTrue("ACME", true);
-            assertTrue("Desert", true);
-            assertTrue("Quality control", true);
-            assertTrue("Persistence", true);
-        }
+        assertTrue(test_job_fields.getName(), true);
+        assertTrue(test_job_fields.getEmployer().getValue(), true);
+        assertTrue(test_job_fields.getLocation().getValue(), true);
+        assertTrue(test_job_fields.getPositionType().getValue(), true);
+        assertTrue(test_job_fields.getCoreCompetency().getValue(), true);
+    }
+
+
 }
